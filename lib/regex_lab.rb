57e8724292020f7beca618c7a16c0c71af13461e
Delia Constantino <dconstan92@gmail.com>
2020-10-26 13:47:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
   if word.match(/^[aeiou]*/) != nil
     return true
-  elsif word.match(/^[bcdfghjklmnpqrstvwxyz]*/) != nil
+  elsif word.match(/^[^]*/) != nil
     return false
   end
 end
