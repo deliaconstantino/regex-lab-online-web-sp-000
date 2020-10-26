3424f7d1a53dccf6d5cfbdb41681432799e7fa3c
Delia Constantino <dconstan92@gmail.com>
2020-10-26 14:53:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiouAEIOU]/) != nil
-    return true
-  else
-    false
-  end
+  word.match(/^[aeiouAEIOU]/) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
